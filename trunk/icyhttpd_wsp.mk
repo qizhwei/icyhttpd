6b552411baa3370ae7140e506d97c0bc0b906d87
iceboy@sjtu.edu.cn <iceboy@sjtu.edu.cn@eea5b2d7-23b5-c2ca-7a99-21362919fd3c>
2011-01-31 09:11:05 +0000

    delete everything

--- trunk/icyhttpd_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ icyhttpd - Debug ]----------
-	@"mingw32-make.exe"  -j 2 -f "icyhttpd.mk"
-clean:
-	@echo ----------Cleaning project:[ icyhttpd - Debug ]----------
-	@"mingw32-make.exe"  -j 2 -f "icyhttpd.mk" clean

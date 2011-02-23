6fd5933c32005f3eca399e14c8131a2397161949
iceboy@iceboy.org <iceboy@iceboy.org@eea5b2d7-23b5-c2ca-7a99-21362919fd3c>
2011-02-23 09:01:07 +0000

    delete old files

--- trunk/conn.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CONN_H
-#define _CONN_H
-
-#include "endpoint.h"
-#include "socket.h"
-
-extern int conn_create(endpoint_t *e, socket_t *s);
-
-#endif
--- trunk/echo_handler.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ECHO_HANDLER
-#define _ECHO_HANDLER
-
-#include "handler.h"
-
-extern handler_t *echo_handler;
-extern void echo_handler_init(void);
-
-#endif
